fad7fc4a3e6f202880e93fb549b34ea52c9d945b
Richard Davey <rich@photonstorm.com>
2020-02-05 22:25:03 +0000

    Export CanvasRenderer

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 //  @namespace Box2DLite
 
 import Body from './Body';
+import CanvasRenderer from './CanvasRenderer';
 import World from './World';
 import Vec2 from './math/Vec2';
 
 export {
     Body,
+    CanvasRenderer,
     World,
     Vec2
 };
