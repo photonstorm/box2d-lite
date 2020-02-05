1929817385b0308243f48d61306f93f05e2d49b5
Richard Davey <rich@photonstorm.com>
2020-02-05 23:01:00 +0000

    Export Joint

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@
 
 import Body from './Body';
 import CanvasRenderer from './CanvasRenderer';
+import Joint from './Joint';
 import World from './World';
 import Vec2 from './math/Vec2';
 
 export {
     Body,
     CanvasRenderer,
+    Joint,
     World,
     Vec2
 };
