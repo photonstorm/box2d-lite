7e88f198c3b508a242a12b0012889063d2727661
Richard Davey <rich@photonstorm.com>
2020-02-06 12:46:49 +0000

    Add terser

--- rollup.examples.config.js
+++ rollup.examples.config.js
@@ -1,13 +1,22 @@
 import typescript from 'rollup-plugin-typescript2';
+import {terser} from 'rollup-plugin-terser';
 
 export default {
 
     input: './examples/index.ts',
 
-    output: {
-        file: './examples/index.js',
-        format: 'esm'
-    },
+    output: [
+        {
+            file: './examples/index.js',
+            format: 'esm'
+        },
+        {
+            file: './examples/index.min.js',
+            format: 'iife',
+            name: 'Box2DLiteTS',
+            plugins: [ terser() ]
+        }
+    ],
 
     plugins: [
         typescript({
