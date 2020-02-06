6aad89933e787d149dfec4e242db65d87d97a918
Richard Davey <rich@photonstorm.com>
2020-02-06 23:47:15 +0000

    Inline Arbiter.preStep

--- src/Arbiter.ts
+++ src/Arbiter.ts
@@ -118,23 +118,51 @@ export default class Arbiter
         {
             let c = contacts[i];
 
-            let r1: Vec2 = Vec2.sub(c.position, body1.position);
-            let r2: Vec2 = Vec2.sub(c.position, body2.position);
+            // let r1: Vec2 = Vec2.sub(c.position, body1.position);
+
+            let r1X = c.positionX - body1.position.x;
+            let r1Y = c.positionY - body1.position.y;
+
+            // let r2: Vec2 = Vec2.sub(c.position, body2.position);
+ 
+            let r2X = c.positionX - body2.position.x;
+            let r2Y = c.positionY - body2.position.y;
 
             //  Precompute normal mass, tangent mass and bias
-            let rn1 = Vec2.dot(r1, c.normal);
-            let rn2 = Vec2.dot(r2, c.normal);
+
+            // let rn1 = Vec2.dot(r1, c.normal);
+            let rn1 = Vec2.dotXY(r1X, r1Y, c.normalX, c.normalY);
+
+            // let rn2 = Vec2.dot(r2, c.normal);
+            let rn2 = Vec2.dotXY(r2X, r2Y, c.normalX, c.normalY);
+
             let normal = body1.invMass + body2.invMass;
 
-            normal += body1.invI * (Vec2.dot(r1, r1) - rn1 * rn1) + body2.invI * (Vec2.dot(r2, r2) - rn2 * rn2);
+            // normal += body1.invI * (Vec2.dot(r1, r1) - rn1 * rn1) + body2.invI * (Vec2.dot(r2, r2) - rn2 * rn2);
+            normal += body1.invI * (Vec2.dotXY(r1X, r1Y, r1X, r1Y) - rn1 * rn1) + body2.invI * (Vec2.dotXY(r2X, r2Y, r2X, r2Y) - rn2 * rn2);
 
             c.massNormal = 1 / normal;
 
-            let tangent = Vec2.crossVS(c.normal, 1);
-            let rt1 = Vec2.dot(r1, tangent);
-            let rt2 = Vec2.dot(r2, tangent);
+            // static crossVS (v: Vec2, s: number): Vec2
+            // {
+            //     return new Vec2(s * v.y, -s * v.x);
+            // }
+
+            // let tangent = Vec2.crossVS(c.normal, 1);
+            let tangentX = c.normalY;
+            let tangentY = -1 * c.normalX;
+
+            // let rt1 = Vec2.dot(r1, tangent);
+            let rt1 = Vec2.dotXY(r1X, r1Y, tangentX, tangentY);
+
+            // let rt2 = Vec2.dot(r2, tangent);
+            let rt2 = Vec2.dotXY(r2X, r2Y, tangentX, tangentY);
+
             let kTangent = body1.invMass + body2.invMass;
-            kTangent += body1.invI * (Vec2.dot(r1, r1) - rt1 * rt1) + body2.invI * (Vec2.dot(r2, r2) - rt2 * rt2);
+
+            // kTangent += body1.invI * (Vec2.dot(r1, r1) - rt1 * rt1) + body2.invI * (Vec2.dot(r2, r2) - rt2 * rt2);
+
+            kTangent += body1.invI * (Vec2.dotXY(r1X, r1Y, r1X, r1Y) - rt1 * rt1) + body2.invI * (Vec2.dotXY(r2X, r2Y, r2X, r2Y) - rt2 * rt2);
 
             c.massTangent = 1 / kTangent;
 
@@ -143,13 +171,38 @@ export default class Arbiter
             if (accumulateImpulses)
             {
                 //  Normal + Friction impulse
-                let P = Vec2.add(Vec2.mulSV(c.Pn, c.normal), Vec2.mulSV(c.Pt, tangent));
+                // let P = Vec2.add(Vec2.mulSV(c.Pn, c.normal), Vec2.mulSV(c.Pt, tangent));
 
-                body1.velocity.sub(Vec2.mulSV(body1.invMass, P));
-                body1.angularVelocity -= body1.invI * Vec2.crossVV(r1, P);
+                // static mulSV (s: number, v: Vec2): Vec2
+                // {
+                //     return new Vec2(s * v.x, s * v.y);
+                // }
 
-                body2.velocity.add(Vec2.mulSV(body2.invMass, P));
-                body2.angularVelocity += body2.invI * Vec2.crossVV(r2, P);
+                let pX = (c.Pn * c.normalX) + (c.Pt * tangentX);
+                let pY = (c.Pn * c.normalY) + (c.Pt * tangentY);
+
+                // body1.velocity.sub(Vec2.mulSV(body1.invMass, P));
+
+                body1.velocity.x -= (body1.invMass * pX);
+                body1.velocity.y -= (body1.invMass * pY);
+
+                // body1.angularVelocity -= body1.invI * Vec2.crossVV(r1, P);
+
+                // static crossVV (vA: Vec2, vB: Vec2): number
+                // {
+                //     return vA.x * vB.y - vA.y * vB.x;
+                // }
+
+                body1.angularVelocity -= body1.invI * (r1X * pY - r1Y * pX);
+
+                // body2.velocity.add(Vec2.mulSV(body2.invMass, P));
+
+                body2.velocity.x += (body2.invMass * pX);
+                body2.velocity.y += (body2.invMass * pY);
+
+                // body2.angularVelocity += body2.invI * Vec2.crossVV(r2, P);
+
+                body2.angularVelocity += body2.invI * (r2X * pY - r2Y * pX);
             }
         }
     }
@@ -166,6 +219,13 @@ export default class Arbiter
         {
             let c = contacts[i];
 
+            c.r1X = c.positionX - body1.position.x;
+            c.r1Y = c.positionY - body1.position.y;
+
+            c.r2X = c.positionX - body2.position.x;
+            c.r2Y = c.positionY - body2.position.y;
+
+            /*
             let r1 = c.r1;
             let r2 = c.r2;
 
@@ -178,6 +238,7 @@ export default class Arbiter
                 c.position.x - body2.position.x,
                 c.position.y - body2.position.y
             );
+            */
 
             //  Relative velocity at contact
 
@@ -193,14 +254,14 @@ export default class Arbiter
             // let subX = addX - body1.velocity.x;
             // let subY = addY - body1.velocity.y;
 
-            let subX = (body2.velocity.x + (-body2.angularVelocity * r2.y)) - body1.velocity.x;
-            let subY = (body2.velocity.y + (body2.angularVelocity * r2.x)) - body1.velocity.y;
+            let subX = (body2.velocity.x + (-body2.angularVelocity * c.r2Y)) - body1.velocity.x;
+            let subY = (body2.velocity.y + (body2.angularVelocity * c.r2X)) - body1.velocity.y;
 
             // let subX = (body2.velocity.x + cross1x) - body1.velocity.x;
             // let subY = (body2.velocity.y + cross1y) - body1.velocity.y;
 
-            let dVx = subX - (-body1.angularVelocity * r1.y);
-            let dVy = subY - (body1.angularVelocity * r1.x);
+            let dVx = subX - (-body1.angularVelocity * c.r1Y);
+            let dVy = subY - (body1.angularVelocity * c.r1X);
             
             // let dVx = subX - cross2x;
             // let dVy = subY - cross2y;
@@ -209,7 +270,8 @@ export default class Arbiter
 
             //  Compute normal impulse
 
-            let vn = Vec2.dotXYV(dVx, dVy, c.normal);
+            // let vn = Vec2.dotXYV(dVx, dVy, c.normal);
+            let vn = Vec2.dotXY(dVx, dVy, c.normalX, c.normalY);
 
             let dPn = c.massNormal * (-vn + c.bias);
 
@@ -227,22 +289,30 @@ export default class Arbiter
 
             //  Apply contact impulse
 
-            let PnX = dPn * c.normal.x;
-            let PnY = dPn * c.normal.y;
+            let PnX = dPn * c.normalX;
+            let PnY = dPn * c.normalY;
 
             body1.velocity.x -= (body1.invMass * PnX);
             body1.velocity.y -= (body1.invMass * PnY);
-            body1.angularVelocity -= body1.invI * Vec2.crossVXY(c.r1, PnX, PnY);
+            body1.angularVelocity -= body1.invI * Vec2.crossXY(c.r1X, c.r1Y, PnX, PnY);
 
             body2.velocity.x += (body2.invMass * PnX);
             body2.velocity.y += (body2.invMass * PnY);
-            body2.angularVelocity += body2.invI * Vec2.crossVXY(c.r2, PnX, PnY);
+            body2.angularVelocity += body2.invI * Vec2.crossXY(c.r2X, c.r2Y, PnX, PnY);
 
             //  Relative velocity at contact
 
-            let tangent = Vec2.crossVS(c.normal, 1);
+            // let tangent = Vec2.crossVS(c.normal, 1);
 
-            let vt = Vec2.dotXYV(dVx, dVy, tangent);
+            // static crossVS (v: Vec2, s: number): Vec2
+            // {
+            //     return new Vec2(s * v.y, -s * v.x);
+            // }
+
+            let tangentX = c.normalY;
+            let tangentY = -1 * c.normalX;
+
+            let vt = Vec2.dotXY(dVx, dVy, tangentX, tangentY);
 
             let dPt = c.massTangent * (-vt);
 
@@ -264,16 +334,16 @@ export default class Arbiter
 
             // Apply contact impulse
 
-            PnX = dPt * tangent.x;
-            PnY = dPt * tangent.y;
+            PnX = dPt * tangentX;
+            PnY = dPt * tangentY;
 
             body1.velocity.x -= (body1.invMass * PnX);
             body1.velocity.y -= (body1.invMass * PnY);
-            body1.angularVelocity -= body1.invI * Vec2.crossVXY(c.r1, PnX, PnY);
+            body1.angularVelocity -= body1.invI * Vec2.crossXY(c.r1X, c.r1Y, PnX, PnY);
 
             body2.velocity.x += (body2.invMass * PnX);
             body2.velocity.y += (body2.invMass * PnY);
-            body2.angularVelocity += body2.invI * Vec2.crossVXY(c.r2, PnX, PnY);
+            body2.angularVelocity += body2.invI * Vec2.crossXY(c.r2X, c.r2X, PnX, PnY);
         }
     }
 }
