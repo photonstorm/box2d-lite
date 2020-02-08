1e0538825ba378831825fe91cda199e2a458b635
Richard Davey <rich@photonstorm.com>
2020-02-08 01:35:15 +0000

    Fixed NaN issue

--- src/math/AABB.ts
+++ src/math/AABB.ts
@@ -5,30 +5,35 @@ export default class AABB
     body: Body;
 
     // center
-    x: number = 0;
-    y: number = 0;
+    x: number;
+    y: number;
 
     // top-left
-    x1: number = 0;
-    y1: number = 0;
+    x1: number;
+    y1: number;
 
     //  bottom-right
-    x2: number = 0;
-    y2: number = 0;
+    x2: number;
+    y2: number;
 
     //  width / height
-    width: number = 0;
-    height: number = 0;
-
-    prevAngle: number;
+    width: number;
+    height: number;
 
     constructor (body: Body)
     {
         this.body = body;
 
-        this.prevAngle = Number.MIN_VALUE;
+        this.x = 0;
+        this.y = 0;
 
-        this.update();
+        this.x1 = 0;
+        this.x2 = 0;
+        this.y1 = 0;
+        this.y2 = 0;
+
+        this.width = 0;
+        this.height = 0;
     }
 
     update ()
@@ -38,44 +43,32 @@ export default class AABB
         this.x = body.position.x;
         this.y = body.position.y;
 
-        if (body.rotation === this.prevAngle)
-        {
-            this.x1 = this.x - (this.width * 0.5);
-            this.x2 = this.x + (this.width * 0.5);
-            this.y1 = this.y - (this.height * 0.5);
-            this.y2 = this.y + (this.height * 0.5);
-        }
-        else
-        {
-            //  The top-right and bottom-right corners (unrotated)
-            const c1x: number = body.width * 0.5;
-            const c2x: number = body.width * 0.5;
-            const c1y: number = -body.height * 0.5;
-            const c2y: number = body.height * 0.5;
+        //  The top-right and bottom-right corners (unrotated)
+        const c1x: number = body.width * 0.5;
+        const c2x: number = body.width * 0.5;
+        const c1y: number = -body.height * 0.5;
+        const c2y: number = body.height * 0.5;
 
-            const sin: number = Math.sin(body.rotation);
-            const cos: number = Math.cos(body.rotation);
+        const sin: number = Math.sin(body.rotation);
+        const cos: number = Math.cos(body.rotation);
 
-            //  Transformed corners
-            const x1x: number = c1x * cos - c1y * sin;
-            const x1y: number = c1x * sin + c1y * cos;
-            const x2x: number = c2x * cos - c2y * sin;
-            const x2y: number = c2x * sin + c2y * cos;
+        //  Transformed corners
+        const x1x: number = c1x * cos - c1y * sin;
+        const x1y: number = c1x * sin + c1y * cos;
+        const x2x: number = c2x * cos - c2y * sin;
+        const x2y: number = c2x * sin + c2y * cos;
 
-            //  Extents
-            const ex: number = Math.max(Math.abs(x1x), Math.abs(x2x));
-            const ey: number = Math.max(Math.abs(x1y), Math.abs(x2y));
+        //  Extents
+        const ex: number = Math.max(Math.abs(x1x), Math.abs(x2x));
+        const ey: number = Math.max(Math.abs(x1y), Math.abs(x2y));
 
-            this.x1 = this.x - ex;
-            this.x2 = this.x + ex;
-            this.y1 = this.y - ey;
-            this.y2 = this.y + ey;
+        this.x1 = this.x - ex;
+        this.x2 = this.x + ex;
+        this.y1 = this.y - ey;
+        this.y2 = this.y + ey;
 
-            this.width = this.x2 - this.x1;
-            this.height = this.y2 - this.y1;
-
-            this.prevAngle = body.rotation;
-        }
+        this.width = this.x2 - this.x1;
+        this.height = this.y2 - this.y1;
     }
 
     isPoint (): boolean
